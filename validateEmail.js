dc9f9705bcb905a662422163427f186e22bd6dc9
irenetjrs <irkapidgorodetska@gmail.com>
2023-03-07 16:53:18 +0200

    task 2

--- validateEmail.js
+++ validateEmail.js
@@ -1,3 +1,21 @@
 function validateEmail(email){
+   if(!email){
+      return false;
+   }
+   if(typeof email !== 'string'){
+      return false;
+   }
+   if (+email === 0){
+      return false;
+   }
+   let checkCharacters = email.split('');
+   if (!checkCharacters.includes('@') 
+      || checkCharacters[0] === '@'){
+      return false;
+   }
+   let checkLength = email.split('');
+   if (checkLength.length > 5){
+   return false
+   }
    return true
 }
--- validateEmail.test.js
+++ validateEmail.test.js
@@ -5,3 +5,33 @@ describe("describe", function() {
 
    });
 })
+
+// task 2
+
+let assert = chai.assert;
+
+describe('validateEmail', function(){
+   it('not an empty string', function(){
+      assert.equal(validateEmail(''), false);
+   });
+   it('typeof string', function() {
+      assert.equal(validateEmail(222), false, 'number error');
+      assert.equal(validateEmail(true), false, 'boolean error');
+      assert.equal(validateEmail({}), false, 'object error');
+      });
+      it('not all spaces', function() {
+      assert.equal(validateEmail('     '), false);
+      });
+      it('Don\'t have the @ or @ the first character', function(){
+      assert.isFalse(validateEmail('emailgmail.com'), 'Don\'t have the @');
+      assert.isFalse(validateEmail('@emailgmail.com'), '@ the first character')
+      });
+   })
+   
+   // task 2
+
+   describe('validateEmail', function(){
+   it('less than 5', function(){
+         assert.isFalse(validateEmail('emailgmail.com'), false);
+      })
+   })
